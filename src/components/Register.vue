0ae265760fc6b4206e831dc65ed9b09a2c8f2d72
Willfrit <remy-voet@outlook.fr>
2019-01-25 17:24:52 +0100

    Validation lib, more clean code.

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -7,62 +7,66 @@
     </b-alert>
     <!-- USERNAME FIELD -->
     <b-form @submit="submitRegister">
-      <b-form-group
-        label="Username :"
-        label-for="usernameRegister"
-        :state="inputError.username.state"
-        :invalid-feedback="inputError.username.msgE"
-      >
+      <b-form-group label="Username :" label-for="usernameRegister">
         <b-form-input
           id="usernameRegister"
           type="text"
-          v-model="input.username"
-          required
+          v-model="form.username"
+          aria-describedby="usernameFeedback"
           placeholder="Username"
-          :state="inputError.username.state"
+          name="username"
+          v-validate="{required:true, min:4, max:20, alpha_num:true}"
+          :state="validateState('username')"
         ></b-form-input>
+        <b-form-invalid-feedback id="usernameFeedback">{{ errors.first('username') }}</b-form-invalid-feedback>
       </b-form-group>
       <!-- EMAIL FIELD -->
       <b-form-group label="Email :" label-for="emailRegister">
         <b-form-input
           id="emailRegister"
           type="email"
-          v-model="input.email"
+          v-model="form.email"
           placeholder="Enter email"
+          aria-describedby="emailFeedback"
+          name="email"
+          v-validate="{email:true}"
+          :state="validateState('email')"
         ></b-form-input>
+        <b-form-invalid-feedback id="emailFeedback">{{ errors.first('email') }}</b-form-invalid-feedback>
       </b-form-group>
       <!-- PASSWORD FIELD -->
-      <b-form-group
-        label="Password :"
-        label-for="passwordRegister1"
-        :state="inputError.password1.state"
-        :invalid-feedback="inputError.password1.msgE"
-      >
+      <b-form-group label="Password :" label-for="passwordRegister1">
         <b-form-input
           id="passwordRegister1"
           type="password"
-          v-model="input.password1"
+          v-model="form.password"
           placeholder="Password"
-          :state="inputError.password1.state"
+          aria-describedby="passwordFeedback"
+          name="password"
+          ref="password"
+          v-validate="{min:6, required:true}"
+          :state="validateState('password')"
         ></b-form-input>
+        <b-form-invalid-feedback id="passwordFeedback">{{ errors.first('password') }}</b-form-invalid-feedback>
       </b-form-group>
       <!-- PASSWORD CONFIRMATION FIELD -->
-      <b-form-group
-        label="Password confirmation :"
-        label-for="passwordRegister2"
-        :state="inputError.password2.state"
-        :invalid-feedback="inputError.password2.msgE"
-      >
+      <b-form-group label="Password confirmation :" label-for="passwordConfRegister">
         <b-form-input
-          id="passwordRegister2"
+          id="passwordConfRegister"
           type="password"
-          v-model="input.password2"
+          v-model="form.passwordConfirmation"
           placeholder="Password confirmation"
-          :state="inputError.password2.state"
+          aria-describedby="passwordConfFeedback"
+          name="Password Confirmation"
+          v-validate="{confirmed:'password', required:true}"
+          :state="validateState('Password Confirmation')"
         ></b-form-input>
+        <b-form-invalid-feedback
+          id="passwordConfFeedback"
+        >{{ errors.first('Password Confirmation') }}</b-form-invalid-feedback>
       </b-form-group>
       <!-- SUBMIT BUTTON -->
-      <b-button variant="primary" type="submit">Register</b-button>
+      <b-button variant="primary" type="submit" :disabled="disableSubmit()">Register</b-button>
     </b-form>
   </div>
 </template>
@@ -74,26 +78,7 @@ export default {
   name: "Register",
   data() {
     return {
-      inputError: {
-        username: {
-          state: null,
-          msgE: ""
-        },
-        password1: {
-          state: null,
-          msgE: ""
-        },
-        password2: {
-          state: null,
-          msgE: ""
-        }
-      },
-      input: {
-        username: "",
-        password1: "",
-        password2: "",
-        email: ""
-      },
+      form: {},
       alerts: {
         error: null
       }
@@ -101,61 +86,45 @@ export default {
   },
   props: {},
   methods: {
+    disableSubmit() {
+      return (
+        this.errors.any() ||
+        Object.keys(this.veeFields).some(key => !this.veeFields[key].valid)
+      );
+    },
+    validateState(ref) {
+      if (
+        this.veeFields[ref] &&
+        (this.veeFields[ref].dirty || this.veeFields[ref].validated)
+      ) {
+        return !this.errors.has(ref);
+      }
+      return null;
+    },
     submitRegister(evt) {
       evt.preventDefault();
-      Object.keys(this.inputError).forEach((key) => {
-        this.inputError[key].state = null
-      })
       this.alerts.error = null;
-      if (this.checkInputs()) {
-        const username = this.input.username;
-        registerAPI(
-          username,
-          this.input.email,
-          this.input.password1,
-          this.input.password2
-        )
-          .then(res => {
-            this.$emit("login", username, res.data.token);
-            this.$router.replace("home");
-          })
-          .catch(error => {
-            // eslint-disable-next-line
-            console.error(error);
-            if (error.response) {
-              this.alerts.error =
-                "Server response : " + error.response.data.errors;
-            } else {
-              this.alerts.error = "Error : " + error.message;
-            }
-          });
-      }
-    },
-    checkInputs() {
-      if (this.input.username.length < 4 || this.input.username.length > 20) {
-        this.inputError.username.state = false;
-        this.inputError.username.msgE =
-          "The <b>Username</b> need to have more than <b>4 caracters!</b>";
-      }
-      if (this.input.password1.length < 6) {
-        this.inputError.password1.state = false;
-        this.inputError.password1.msgE =
-          "The password need to have more than 5 characters";
-      }
-      if (this.input.password1 !== this.input.password2) {
-        this.inputError.password2.state = false;
-        this.inputError.password2.msgE = "It is not the same than above";
-      }
-
-      if (
-        !Object.keys(this.inputError).every(key => {
-          return this.inputError[key].state || this.inputError[key].state === null;
+      const username = this.form.username;
+      registerAPI(
+        username,
+        this.form.email,
+        this.form.password,
+        this.form.passwordConfirmation
+      )
+        .then(res => {
+          this.$emit("login", username, res.data.token);
+          this.$router.replace("home");
         })
-      ) {
-        this.alerts.error = "Some inputs aren't valid => see details below";
-        return false;
-      }
-      return true;
+        .catch(error => {
+          // eslint-disable-next-line
+          console.error(error);
+          if (error.response) {
+            this.alerts.error =
+              "Server response : " + error.response.data.errors;
+          } else {
+            this.alerts.error = "Error : " + error.message;
+          }
+        });
     }
   }
 };
--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
+import VeeValidate from 'vee-validate';
 import router from './router'
 
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
+Vue.use(VeeValidate, {
+  // important to name this something other than 'fields'
+  // Otherwise conflict with bootstrap-vue 
+  // https://bootstrap-vue.netlify.com/docs/reference/validation/
+  fieldsBagName: 'veeFields',
+})
 
 new Vue({
   render: h => h(App),
