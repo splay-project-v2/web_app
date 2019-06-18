a9cb59ec3d9f00e015a120974b1c8bfe519fdd8b
Samuel Monroe <spat.monroe@gmail.com>
2019-06-18 13:03:37 +0200

    Smalle typos fix

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,30 +5,30 @@
     <hr class="my-4">
     <h3>Overview</h3>
     <div>
-      Splay simplifies the prototyping and development of large-scale distributed applications and overlay networks. 
+      Splay simplifies the prototyping and development of large-scale distributed applications and overlay networks.
       Splay covers the complete chain of distributed system design, development and testing: from coding and local runs
-      to controlled deployment, experiment control and monitoring. Splay allows developers to specify their distributed 
-      applications in a concise way using a specialized language based on Lua, a highly-efficient embeddable scripting 
+      to controlled deployment, experiment control and monitoring. Splay allows developers to specify their distributed
+      applications in a concise way using a specialized language based on Lua, a highly-efficient embeddable scripting
       language. Splay applications execute in a safe environment with restricted access to local resources (file system, network, memory)
       and can be instantiated on a large variety of testbeds composed a large set of nodes with a single command.
     </div>
     <h3>Getting started</h3>
     <div>
-      The main research paper that describes Splay, evaluates its performances and presents several 
-      typical experiments has been published in the proceedings of the 6th USENIX Symposium on Networked
+      The main research paper, which describes Splay evaluates its performances and presents several
+      typical experiments, has been published in the proceedings of the 6th USENIX Symposium on Networked
       Systems Design and Implementation (NSDI'09).
 
       SplayNet implements topology emulation features for Splay.
       It has been published in the Proceedings of the 14th ACM/IFIP/USENIX International Middleware Conference (MIDDLEWARE'13).
 
-      The SPLAY NSDI'09 paper is available as a 
+      The SPLAY NSDI'09 paper is available as a
       <a target="_blank" href="https://www.usenix.org/legacy/event/nsdi09/tech/full_papers/leonini/leonini_html/">web page</a>.
 
-      The SplayNet MIDDLEWARE'13 is available as 
+      The SplayNet MIDDLEWARE'13 is available as
       <a target="_blank" href="http://members.unine.ch/valerio.schiavoni/publications/splaynet_middleware13.pdf">PDF</a>.
 
       A more recent version of SPLAY (Version 2) has been written with new technologies or more recent version.
-      Also, the structure has been simplified and improved. Check the new 
+      Also, the structure has been simplified and improved. Check the new
       <a href="https://github.com/splay-project-v2" target="_blank">Github Organisation</a>.
     </div>
     <h3>More information</h3>
